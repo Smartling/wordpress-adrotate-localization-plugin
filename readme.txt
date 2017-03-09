430c5397aed655991d81b90904ca5359e2ae7bb0
Dmitriy Studinskiy <dms@smartling.com>
2017-03-09 19:35:46 +0200

    Added readme.txt for publishing on on marketplace

---
+++ readme.txt
@@ -0,0 +1,47 @@
+=== Smartling AdRotate localization ===
+
+Contributors: smartling
+Tags: automation, international, internationalisation, internationalization, localisation, localization, multilingual, smartling, translate, translation, adrotate, adrotator, ads
+Requires at least: 4.6
+Tested up to: 4.7.3
+Stable tag: 1.0
+License: GPL-3.0 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.txt
+
+Extend Smartling Connector functionality by adding localization for AdRotate plugin
+
+== Description ==
+
+[The Smartling Connector](https://wordpress.org/plugins/smartling-connector) extends the WordPress interface for seamless management of the translation process. This plugin allows to localize [AdRotate](https://wordpress.org/plugins/adrotate/) adverts and groups
+
+Integration Features
+
+* Automatic change detection for content updates
+* Robust custom workflow engine configurable per language
+* Automatic download of completed translations to WordPress
+* Translation Memory integration
+* No tie-ins to translation agencies or vendors
+* Reporting for translation velocity, efficiency
+
+== Installation ==
+
+= Minimum Requirements =
+* WordPress 4.6 or higher
+* [Smartling Connector](https://wordpress.org/plugins/smartling-connector) 4.1 or higher
+
+1. Upload the plugin files to the `/wp-content/plugins/smartling-adrotate-localization` directory, or install the plugin through the WordPress plugins screen directly.
+1. Go to the Plugins screen and **Network Activate** the AdRotate localization plugin.
+
+== Frequently Asked Questions ==
+
+Additional information on the Smartling Connector for WordPress can be found [there](http://help.smartling.com/knowledge-base/sections/wordpress-connector/).
+
+== Screenshots ==
+
+1. The Bulk Submit page allows submission of standard WordPress assets including adverts and groups for all configured locales from a single interface.
+2. Track translation status within WordPress from the Submissions Board. View overall progress of submitted translation requests as well as resend updated content.
+
+== Changelog ==
+
+= 1.0 =
+The initial release. It allows to translate adverts and groups
--- smartling-adrotate.php
+++ smartling-adrotate.php
@@ -1,20 +1,20 @@
 <?php
 
 /**
- * @link              https://www.smartling.com
- * @since             1.0.0
- * @package           smartling-adrotate
- * @wordpress-plugin
- * Plugin Name:       Smartling Adrotate Extension
- * Plugin URI:        https://www.smartling.com/translation-software/wordpress-translation-plugin/
- * Description:       Integrate your Wordpress site with Smartling to upload your content and download translations.
- * Version:           1.0.0
- * Author:            Smartling
- * Author URI:        https://www.smartling.com
- * License:           GPL-2.0+
- * Network:           true
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- */
+  * @link              https://www.smartling.com
+  * @since             1.0.0
+  * @package           smartling-adrotate-localization
+  * @wordpress-plugin
+  * Plugin Name:       Smartling AdRotate localization
+  * Description:       Extend Smartling Connector functionality to support localization of AdRotate adverts and groups
+  * Plugin URI:        https://www.smartling.com/translation-software/wordpress-translation-plugin/
+  * Author URI:        https://www.smartling.com
+  * License:           GPL-3.0+
+  * Network:           true
+  * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+  * Version: 1.0
+*/
+
 use SmartlingAdrotate\ContentTypes\ContentTypeAdrotateAd;
 use SmartlingAdrotate\ContentTypes\ContentTypeAdrotateGroups;
 use SmartlingAdrotate\ContentTypes\ContentTypeAdrotateLinkmeta;
@@ -31,17 +31,17 @@ add_action('plugins_loaded', function () {
             require_once __DIR__ . '/content_entities/AdrotateGroupsEntity.php';
             require_once __DIR__ . '/content_entities/AdrotateScheduleEntity.php';
             require_once __DIR__ . '/content_entities/AdrotateLinkmetaEntity.php';
-            
+
             // Reference processor.
             require_once __DIR__ . '/AdrotateReferencedContentProcessor.php';
-            
+
             // Content Types.
             require_once __DIR__ . '/content_types/ContentTypeAdrotateBasic.php';
             require_once __DIR__ . '/content_types/ContentTypeAdrotateAd.php';
             require_once __DIR__ . '/content_types/ContentTypeAdrotateGroups.php';
             require_once __DIR__ . '/content_types/ContentTypeAdrotateSchedule.php';
             require_once __DIR__ . '/content_types/ContentTypeAdrotateLinkmeta.php';
-            
+
             // Adrotate bootstrap.
             ContentTypeAdrotateAd::register($di);
             ContentTypeAdrotateGroups::register($di);
